8bd409fa2bbb0b73728e8633bc3d6fead1fe1d9b
Aleksey Petras <leha.petras1998@gmail.com>
2023-03-16 14:31:46 +0300

    create gendiff.php witch containe discription for bin/gendiff

--- src/gendiff.php
+++ src/gendiff.php
@@ -1,1 +1,21 @@
 <?php
+function gendiff()
+{
+    $doc = <<<DOC
+Generate diff
+
+Usage: 
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help        Show this screen.
+  -v --version     Show version.
+
+DOC;
+
+
+    $result = Docopt::handle($doc, array('version' => '1.0.0rc2'));
+    foreach ($result as $k => $v)
+        echo $k . ': ' . json_encode($v) . PHP_EOL;
+}
